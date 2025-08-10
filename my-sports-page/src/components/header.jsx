0c5a707f04c0c7ed8f2ab94a2ea1ca75f3ab64c4
mortalprotagonist <aktr2003@gmail.com>
2025-08-11 03:44:37 +0530

    Started Basic UI

---
+++ my-sports-page/src/components/header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+return(
+    <header>
+        <h1>CEP Sports Hub</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Matches</a></li>
+                <li><a href="#">Calculator</a></li>
+            </ul>
+        </nav>
+    </header>
+);
+
+}
+
+export default Header;
